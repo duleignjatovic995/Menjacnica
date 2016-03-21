128ae732c9be40637d62ef32a028dffe1433cbd6
duleignjatovic995 <dusan.ignjatovic@hotmail.com>
2016-03-22 00:13:53 +0100

    Override-ovane toString, equals i hashCode metode u klase Valuta i DnevniKurs

--- Menjacnica/src/valuta/DnevniKurs.java
+++ Menjacnica/src/valuta/DnevniKurs.java
@@ -2,6 +2,10 @@ package valuta;
 
 import java.util.GregorianCalendar;
 
+/**
+ * @author Dule
+ *
+ */
 public class DnevniKurs {
 
 	GregorianCalendar datum;
@@ -40,5 +44,54 @@ public class DnevniKurs {
 	public void setSrednjiKurs(double srednjiKurs) {
 		this.srednjiKurs = srednjiKurs;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((valuta == null) ? 0 : valuta.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DnevniKurs other = (DnevniKurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		if (valuta == null) {
+			if (other.valuta != null)
+				return false;
+		} else if (!valuta.equals(other.valuta))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "DnevniKurs: datum: " + datum + ", valuta:" + valuta + ", prodajniKurs:" + prodajniKurs + ", kupovniKurs:"
+				+ kupovniKurs + ", srednjiKurs:" + srednjiKurs;
+	}
+	
+	
 	
 }
