c7ce73d3838574bbdbe3ca3729a646ae7b7793cf
duleignjatovic995 <dusan.ignjatovic@hotmail.com>
2016-03-22 02:26:21 +0100

    Prethodno napravljena klasa DnevniKursKolekcija sada implementira interfejs IMenjacnica ali su metode nefunkcionalne.

--- Menjacnica/src/valuta/DnevniKursKolekcija.java
+++ Menjacnica/src/valuta/DnevniKursKolekcija.java
@@ -1,14 +1,35 @@
 package valuta;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
 
-public class DnevniKursKolekcija {
+import valuta.interfejs.IMenjacnica;
+
+public class DnevniKursKolekcija implements IMenjacnica{
 
 	List<DnevniKurs> listaKurseva;
 
 	public DnevniKursKolekcija() {
 		listaKurseva = new LinkedList<>();
+	}
+
+	@Override
+	public void dodajDnevniKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiDnevniKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<DnevniKurs> pronadjiKurs(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
